19b4bd2a203a21f93a35fca85424465eb43f1c9a
Taha ÖZMEN <139132956+tahaozmn@users.noreply.github.com>
2023-10-08 17:30:02 +0300

    Add files via upload

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,30 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Taha Özmen on 19.09.2023.
+//
+
+import Foundation
+import UIKit
+
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+          
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+        
+        
+    }
+    
+    
+    
+}
+
---
+++ SimpsonBook/detailsVC.swift
@@ -0,0 +1,30 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by Taha Özmen on 19.09.2023.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    var selectedSimpson : Simpson?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = selectedSimpson?.name
+        jobLabel.text = selectedSimpson?.job
+        imageView.image = selectedSimpson?.image
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
